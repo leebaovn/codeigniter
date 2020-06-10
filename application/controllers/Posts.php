6c5874e3b894b06e4536e3b20dbfd23323b366d1
Lee Bao <leebao101@gmail.com>
2020-06-10 10:50:23 +0700

    add css

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -11,7 +11,7 @@
 			// Init Pagination
 			$this->pagination->initialize($config);
 
-			$data['title'] = 'Latest Posts';
+			$data['title'] = 'New Posts';
 
 			$data['posts'] = $this->post_model->get_posts(FALSE, $config['per_page'], $offset);
 
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,35 +2,49 @@
 	<head>
 		<title>ciBlog</title>
 		<!-- <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css"> -->
-    <link rel="stylesheet" href="<?php echo base_url();?>/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
     <script src="http://cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
+    <link href="<?php echo base_url();?>assets/css/media_query.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo base_url();?>assets/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="<?php echo base_url();?>assets/css/animate.css" rel="stylesheet" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/css/owl.carousel.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo base_url();?>assets/css/owl.theme.default.css" rel="stylesheet" type="text/css"/>
+    <!-- Bootstrap CSS -->
+    <link href="<?php echo base_url();?>assets/css/style_1.css" rel="stylesheet" type="text/css"/>
+    <!-- Modernizr JS -->
+    <script src="<?php echo base_url();?>assets/js/modernizr-3.5.0.min.js"></script>
 	</head>
 	<body>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="<?php echo base_url(); ?>">F9 Blog</a>
-        </div>
-        <div class="collapse navbar-collapse" id="navbarColor01">
-          <ul class="navbar-nav mr-auto">
-             <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts">Bài viết</a></li>
-             <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>categories">Danh mục</a></li>
-          </ul>
-          <ul class="navbar-nav">
-          <?php if(!$this->session->userdata('logged_in')) : ?>
-            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/login">Đăng nhập</a></li>
-            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/register">Đăng ký</a></li>
-          <?php endif; ?>
-          <?php if($this->session->userdata('logged_in')) : ?>
-            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts/create">Tạo post mới</a></li>
-            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>categories/create">Tạo danh mục</a></li>
-            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/logout">Đăng xuất</a></li>
-          <?php endif; ?>
-          </ul>
-        </div>
-      </div>
-    </nav>
+	<div class="container-fluid bg-faded fh5co_padd_mediya padding_786">
+    <div class="container padding_786">
+        <nav class="navbar navbar-toggleable-md navbar-light ">
+            <button class="navbar-toggler navbar-toggler-right mt-3" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation"><span class="fa fa-bars"></span></button>
+            <a class="navbar-brand" href="#"><img src="images/logo.png" alt="img" class="mobile_logo_width"/></a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="<?php echo base_url();?>">Trang chủ</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="<?php echo base_url();?>posts">Bài viết</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="<?php echo base_url();?>users/login">Đăng nhập</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="<?php echo base_url();?>users/register">Đăng ký</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
 
     <div class="container">
       <!-- Flash messages -->
