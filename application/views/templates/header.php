a91289302dc4c7970f8b94b3009c22930d4371e4
Lee Bao <leebao101@gmail.com>
2020-06-10 11:21:00 +0700

    complete header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,11 +1,10 @@
 <html>
-	<head>
+<head>
 		<title>ciBlog</title>
 		<!-- <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css"> -->
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
     <script src="http://cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
-<<<<<<< HEAD
     <link href="<?php echo base_url();?>assets/css/media_query.css" rel="stylesheet" type="text/css"/>
     <link href="<?php echo base_url();?>assets/css/bootstrap.css" rel="stylesheet" type="text/css"/>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
@@ -18,124 +17,10 @@
     <link href="<?php echo base_url();?>assets/css/style_1.css" rel="stylesheet" type="text/css"/>
     <!-- Modernizr JS -->
     <script src="<?php echo base_url();?>assets/js/modernizr-3.5.0.min.js"></script>
-	</head>
-	<body>
-	<div class="container-fluid bg-faded fh5co_padd_mediya padding_786">
-    <div class="container padding_786">
-        <nav class="navbar navbar-toggleable-md navbar-light ">
-            <button class="navbar-toggler navbar-toggler-right mt-3" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation"><span class="fa fa-bars"></span></button>
-            <a class="navbar-brand" href="#"><img src="images/logo.png" alt="img" class="mobile_logo_width"/></a>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="<?php echo base_url();?>">Trang chủ</a>
-                    </li>
-                    <li class="nav-item ">
-                        <a class="nav-link" href="<?php echo base_url();?>posts">Bài viết</a>
-                    </li>
-                    <li class="nav-item ">
-                        <a class="nav-link" href="<?php echo base_url();?>users/login">Đăng nhập</a>
-                    </li>
-                    <li class="nav-item ">
-                        <a class="nav-link" href="<?php echo base_url();?>users/register">Đăng ký</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>
-</div>
+</head>
 
-=======
-    <link href="css/media_query.css" rel="stylesheet" type="text/css"/>
-    <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link href="css/animate.css" rel="stylesheet" type="text/css"/>
-    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
-    <link href="css/owl.carousel.css" rel="stylesheet" type="text/css"/>
-    <link href="css/owl.theme.default.css" rel="stylesheet" type="text/css"/>
-    <!-- Bootstrap CSS -->
-    <link href="css/style_1.css" rel="stylesheet" type="text/css"/>
-    <!-- Modernizr JS -->
-    <script src="js/modernizr-3.5.0.min.js"></script>
-	</head>
-	<body>
-	<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="<?php echo base_url(); ?>">F9 Blog</a>
-        </div>
-        <div class="collapse navbar-collapse" id="navbarColor01">
-          <ul class="navbar-nav mr-auto">
-             <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts">Bài viết</a></li>
-             <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>categories">Danh mục</a></li>
-          </ul>
-          <ul class="navbar-nav">
-          <?php if(!$this->session->userdata('logged_in')) : ?>
-            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/login">Đăng nhập</a></li>
-            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/register">Đăng ký</a></li>
-          <?php endif; ?>
-          <?php if($this->session->userdata('logged_in')) : ?>
-            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts/create">Tạo post mới</a></li>
-            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>categories/create">Tạo danh mục</a></li>
-            <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/logout">Đăng xuất</a></li>
-          <?php endif; ?>
-          </ul>
-        </div>
-      </div>
-    </nav> -->
-    <div class="container-fluid fh5co_header_bg">
-    <div class="container">
-        <div class="row">
-            <div class="col-12 fh5co_mediya_center"><a href="#" class="color_fff fh5co_mediya_setting"><i
-                    class="fa fa-clock-o"></i>&nbsp;&nbsp;&nbsp;Friday, 5 January 2018</a>
-                <div class="d-inline-block fh5co_trading_posotion_relative"><a href="#" class="treding_btn">Trending</a>
-                    <div class="fh5co_treding_position_absolute"></div>
-                </div>
-                <a href="#" class="color_fff fh5co_mediya_setting">Instagram’s big redesign goes live with black-and-white app</a>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="container-fluid">
->>>>>>> ba968243b04bc0f217f7e5a76e3715c0e0338946
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-md-3 fh5co_padding_menu">
-                <img src="images/logo.png" alt="img" class="fh5co_logo_width"/>
-            </div>
-            <div class="col-12 col-md-9 align-self-center fh5co_mediya_right">
-                <div class="text-center d-inline-block">
-                    <a class="fh5co_display_table"><div class="fh5co_verticle_middle"><i class="fa fa-search"></i></div></a>
-                </div>
-                <div class="text-center d-inline-block">
-                    <a class="fh5co_display_table"><div class="fh5co_verticle_middle"><i class="fa fa-linkedin"></i></div></a>
-                </div>
-                <div class="text-center d-inline-block">
-                    <a class="fh5co_display_table"><div class="fh5co_verticle_middle"><i class="fa fa-google-plus"></i></div></a>
-                </div>
-                <div class="text-center d-inline-block">
-                    <a href="https://twitter.com/fh5co" target="_blank" class="fh5co_display_table"><div class="fh5co_verticle_middle"><i class="fa fa-twitter"></i></div></a>
-                </div>
-                <div class="text-center d-inline-block">
-                    <a href="https://fb.com/fh5co" target="_blank" class="fh5co_display_table"><div class="fh5co_verticle_middle"><i class="fa fa-facebook"></i></div></a>
-                </div>
-                <!--<div class="d-inline-block text-center"><img src="images/country.png" alt="img" class="fh5co_country_width"/></div>-->
-                <div class="d-inline-block text-center dd_position_relative ">
-                    <select class="form-control fh5co_text_select_option">
-                        <option>English </option>
-                        <option>French </option>
-                        <option>German </option>
-                        <option>Spanish </option>
-                    </select>
-                </div>
-                <div class="clearfix"></div>
-            </div>
-        </div>
-    </div>
-</div>
+
+<body>
 <div class="container-fluid bg-faded fh5co_padd_mediya padding_786">
     <div class="container padding_786">
         <nav class="navbar navbar-toggleable-md navbar-light ">
@@ -146,35 +31,23 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="<?php echo base_url(); ?>">Trang chủ</a>
                     </li>
                     <li class="nav-item ">
-                        <a class="nav-link" href="blog.html">Blog <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="<?php echo base_url(); ?>posts">Bài viết</a>
                     </li>
                     <li class="nav-item ">
-                        <a class="nav-link" href="single.html">Single <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="<?php echo base_url(); ?>categories">Danh mục</a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuButton2" data-toggle="dropdown"
-                           aria-haspopup="true" aria-expanded="false">World <span class="sr-only">(current)</span></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink_1">
-                            <a class="dropdown-item" href="#">Action in</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuButton3" data-toggle="dropdown"
-                           aria-haspopup="true" aria-expanded="false">Community<span class="sr-only">(current)</span></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink_1">
-                            <a class="dropdown-item" href="#">Action in</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li class="nav-item ">
-                        <a class="nav-link" href="Contact_us.html">Contact <span class="sr-only">(current)</span></a>
-                    </li>
+                    <?php if(!$this->session->userdata('logged_in')) : ?>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/login">Đăng nhập</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/register">Đăng ký</a></li>
+                    <?php endif; ?>
+                    <?php if($this->session->userdata('logged_in')) : ?>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts/create">Tạo post mới</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>users/logout">Đăng xuất</a></li>
+                    <?php endif; ?>
+                    
                 </ul>
             </div>
         </nav>
